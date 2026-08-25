30fc26445bbd3cbb1c0110d8a3e69a02812e1a02
Khushi Sharma <Khushi94575@gmail.com>
2026-08-25 18:32:13 +0530

    Technician

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,17 +2,8 @@ const express = require("express");
 const app = express();
 require("dotenv").config();
 const cookieParser = require("cookie-parser");
-const technicianRoutes = require("./routes/technician.routes");
+
 app.use(express.json());
 app.use(cookieParser());
 
-app.use("/api/technicians", technicianRoutes);
-app.use((err, req, res, next) => {
-  const statusCode = err.statusCode || 500;
-
-  res.status(statusCode).json({
-    success: false,
-    message: err.message || "Internal Server Error",
-  });
-});
 module.exports = app;
