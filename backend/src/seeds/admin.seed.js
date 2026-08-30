89e6c729a6a2a2ac4fa7bd2cadad0e0fce875ac8
kartikshr01 <kartiksharma2462092@gmail.com>
2026-08-30 18:23:07 +0530

    new commit

--- backend/src/seeds/admin.seed.js
+++ backend/src/seeds/admin.seed.js
@@ -11,7 +11,7 @@ const seedAdmin = async () => {
 
     console.log("MongoDB connected");
 
-    const adminEmail = "admin@fixit.com";
+    const adminEmail = "testAdmin@ResolveX";
 
     const existingAdmin = await User.findOne({
       email: adminEmail,
@@ -28,7 +28,7 @@ const seedAdmin = async () => {
     );
 
     await User.create({
-      name: "FixIt Admin",
+      name: "Test Admin",
       email: adminEmail,
       passwordHash,
       role: "ADMIN",
