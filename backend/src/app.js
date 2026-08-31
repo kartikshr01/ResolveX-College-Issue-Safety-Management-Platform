a6b088a6c87ac4600067485f70634e361142e797
Damini Gidwani <daminigidwani13@gmail.com>
2026-08-31 10:55:01 +0530

    add cors in app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
 require("dotenv").config();
-
+const cors = require("cors");
 const errorHandler = require("../src/middleware/errorHandler");
 
 const authRouter = require("../src/routes/auth.routes");
@@ -12,6 +12,13 @@ const notificationRoutes = require("./routes/notification.routes");
 const activityRoutes = require("./routes/activity.routes")
 
 const app = express();
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  }),
+);
 
 app.use(express.json());
 app.use(cookieParser());
