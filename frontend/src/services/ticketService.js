e4e7b31b52d8512617b6cb0f90ce8dbb414059be
kartikshr01 <kartiksharma2462092@gmail.com>
2026-09-05 09:54:49 +0530

    Ticket status workflow fixed

--- frontend/src/services/ticketService.js
+++
@@ -1,9 +0,0 @@
-import api from "../api/axios";
-
-export const getTicketForNotification = async (ticketId) => {
-  const response = await api.get(
-    `/tickets/notification/${ticketId}`,
-  );
-
-  return response.data?.data;
-};
