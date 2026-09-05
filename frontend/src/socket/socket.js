629e4f604b959dc562623128e07c01565a4faa32
kartikshr01 <kartiksharma2462092@gmail.com>
2026-09-05 10:35:24 +0530

    New fixes

--- frontend/src/socket/socket.js
+++ frontend/src/socket/socket.js
@@ -1,5 +1,17 @@
 import { io } from "socket.io-client";
 
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://resolve-x-college-issue-safety-mana.vercel.app",
+];
+
+const io = new Server(server, {
+  cors: {
+    origin: allowedOrigins,
+    credentials: true,
+  },
+});
+
 const socket = io("https://resolvex-backend-01f9.onrender.com/api", {
   withCredentials: true,
   // autoConnect: true,
